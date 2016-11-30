a3d7bc36315b785090c902da3ccdb09320b71039
manueljun17 <manuelmanansalajr@gmail.com>
2016-11-30 14:55:38 +0800

    add chat functionality inside room page

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -5,15 +5,17 @@ import * as xInterface from '../app.interface';
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styleUrls: ['./room.component.css']
+  styleUrls: ['./room.component.scss']
 })
 export class RoomComponent {
   myRoomname: string;
   inputMessage: string;
+  listMessage: xInterface.MESSAGELIST = <xInterface.MESSAGELIST> {};
   constructor( private router: Router,
   private vc: VideocenterService ) {
     this.initialize();
     this.joinRoom();
+    this.listenEvents();
    }
   /**
   *@desc This method will initialize 
@@ -21,6 +23,7 @@ export class RoomComponent {
   */
   initialize() {
     this.inputMessage = '';
+    if ( this.listMessage[0] === void 0 ) this.listMessage[0] = { messages: [] };
   }
   /**
   *@desc This method will get roomname then join the roomname
@@ -32,9 +35,50 @@ export class RoomComponent {
     });
   }
   /**
-   *@desc This method will Leave the room and go back to lobby
-   */
+  *@desc Group of View Method
+  */
+
+  /**
+  *@desc This method will send the message to the server
+  *after that it will empty the message input box
+  *@param message 
+  */  
+  onSendMessage(message: string) {
+    if(message != "") this.vc.sendMessage(message, ()=> { 
+      this.inputMessage = ''; 
+    });
+  }
+  /**
+  *@desc This method will Leave the room and go back to lobby
+  */
   onClickLobby() {
     this.router.navigate(['lobby']);
+  }
+
+  /**
+  *@desc This method will subscribe to all events
+  */
+  listenEvents() {
+    this.vc.myEvent.subscribe( item => {
+      // if( item.eventType == "update-username")this.updateUserOnUserList( item );
+      // if( item.eventType == "join-room") this.onJoinRoomEvent( item );
+      // if( item.eventType == "leave-room") this.onLeaveRoomEvent( item );
+      if( item.eventType == "chatMessage") this.addMessage( item );
+      // if( item.eventType == "log-out") this.removeUserList( item );
+      // if( item.eventType == "disconnect") this.onDisconnectEvent( item );
+    });
+  }
+  /**
+  *@desc Groups of onevent Method 
+  */
+
+  /**
+   *@desc Add to listMessage to be displayed in the view
+   *@param message 
+   */  
+  addMessage( message ) {
+    this.listMessage[0].messages.push( message );
+    let data = { eventType: "scroll-to-bottom"};
+    setTimeout(()=>{ this.vc.myEvent.emit(data); }, 100); 
   }
 }
